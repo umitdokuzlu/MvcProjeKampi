5a076a138da48a02bfa36d08cf60763d87dfe91d
umitdokuzlu <umut-dokuzlu@hotmail.com>
2022-12-06 16:52:46 +0300

    Add project files.

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator:AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar Adını Boş Geçemezsiniz");
+            RuleFor(x => x.WriterSurname).NotEmpty().WithMessage("Yazar Soyadını Boş Geçemezsiniz");
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkımda kısmını Boş Geçemezsiniz");
+            RuleFor(x => x.WriterTitle).NotEmpty().WithMessage("Unvan kısmını Boş Geçemezsiniz");
+            RuleFor(x => x.WriterSurname).MinimumLength(2).WithMessage("Lütfen en az 2 karakter girişi yapın");
+            RuleFor(x => x.WriterSurname).MaximumLength(50).WithMessage("Lütfen 20 karakterden fazla değer girişi yapmayınız");
+        }
+    }
+}
---
+++ MvcProjeKampi/Controllers/ContentController.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class ContentController : Controller
+    {
+        ContentManager cm=new ContentManager(new EfContentDal());
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult GetAllContent(string p)
+        {
+            var values =cm.GetList(p);
+            return View(values.ToList());
+        }
+
+        public ActionResult ContentByHeading(int id)
+        {
+            var contentvalues=cm.GetListByHeadingID(id);
+            return View(contentvalues);
+        }
+    }
+}
